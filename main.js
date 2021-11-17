cacafa95d6dcea761ae30f748fc8810e545c9169
Kelechi Molokwu <km1519@live.mdx.ac.uk>
2021-11-17 18:23:35 +0100

    created store & route, initialized vue

--- main.js
+++ main.js
@@ -52,3 +52,59 @@ const Cart = {
     },
   },
 }
+
+// Store (vuex)
+const store = new Vuex.Store({
+    state: {
+      lessons: [...lessons],
+      cart: [],
+    },
+    mutations: {
+      addToCart(state, lessonItem) {
+        const index = state.lessons.findIndex((item) => item.subject === lessonItem.subject)
+        const isLessonInCart = state.cart.some((item) => item.subject === lessonItem.subject)
+  
+        state.lessons[index].space -= 1
+  
+        if (isLessonInCart) {
+          const cartIndex = state.cart.findIndex((item) => item.subject === lessonItem.subject)
+          state.cart[cartIndex].item += 1
+        } else {
+          state.lessons[index].item += 1
+          state.cart.push(state.lessons[index])
+        }
+      },
+      removeCart(state, cartItem) {
+        const index = state.cart.findIndex((item) => item.subject === cartItem.subject)
+        const lessonIndex = state.lessons.findIndex((item) => item.subject === cartItem.subject)
+  
+        state.lessons[lessonIndex].space += cartItem.item
+  
+        state.cart[index].item > 0 ? (state.cart[index].item -= 1) : (state.cart[index].item = 0)
+        state.cart.splice(index, 1)
+      },
+      resetCart(state) {
+        state.cart = []
+      },
+    },
+  })
+  
+  // Route
+  const routes = [
+    {path: "", component: Home},
+    {path: "/cart", component: Cart},
+  ]
+  
+  const router = new VueRouter({
+    mode: "history",
+    routes,
+  })
+  
+  // Vue-toasted
+  Vue.use(Toasted)
+  
+  // Initialize Vue
+  new Vue({
+    store,
+    router,
+  }).$mount("#app")
