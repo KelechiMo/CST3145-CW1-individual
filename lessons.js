6e544f5ebcddcfa4a2e8df4d7532b932480f4487
Kelechi Molokwu <km1519@live.mdx.ac.uk>
2021-11-17 15:56:23 +0100

    Added lessons to lessons.js

---
+++ lessons.js
@@ -0,0 +1,84 @@
+const lessons = [
+    {
+        "subject": "Math",
+        "location": "London",
+        "price": "£60",
+        "space": 5,
+        "image": "",
+        "item": 0
+    },
+    {
+        "subject": "English",
+        "location": "London",
+        "price": "£50",
+        "space": 5,
+        "image": "",
+        "item": 0
+    },
+    {
+        "subject": "Physics",
+        "location": "London",
+        "price": "£90",
+        "space": 5,
+        "image": "",
+        "item": 0
+    },
+    {
+        "subject": "Sociology",
+        "location": "London",
+        "price": "£70",
+        "space": 5,
+        "image": "",
+        "item": 0
+    },
+    {
+        "subject": "Art",
+        "location": "London",
+        "price": "£100",
+        "space": 5,
+        "image": "",
+        "item": 0
+    },
+    {
+        "subject": "Chemistry",
+        "location": "London",
+        "price": "£60",
+        "space": 5,
+        "image": "",
+        "item": 0
+    },
+    {
+        "subject": "Psycology",
+        "location": "London",
+        "price": "£110",
+        "space": 5,
+        "image": "",
+        "item": 0
+    },
+    {
+        "subject": "Physiology",
+        "location": "London",
+        "price": "£50",
+        "space": 5,
+        "image": "",
+        "item": 0
+    },
+    {
+        "subject": "Biology",
+        "location": "London",
+        "price": "£30",
+        "space": 5,
+        "image": "",
+        "item": 0
+    },
+    {
+        "subject": "Bioinformatics",
+        "location": "London",
+        "price": "£100",
+        "space": 5,
+        "image": "",
+        "item": 0
+    }
+]
+
+export default lessons
