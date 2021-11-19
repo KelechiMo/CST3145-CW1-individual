576cd7834385b7a42b1620f0913df2b7a7d56b50
Kelechi Molokwu <km1519@live.mdx.ac.uk>
2021-11-19 02:39:59 +0100

    Added sort functionality

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ import lessons from "./lessons.js"
 // Components
 const Home = {
   template: "#home",
+  data() {
+    return {
+      sortBy: "",
+      sortBy2: "",
+      reversed: false
+    }
+  },
   computed: {
     lessons() {
       return this.$store.state.lessons
@@ -11,6 +18,72 @@ const Home = {
   methods: {
     addToCart(item) {
       this.$store.commit("addToCart", item)
+    },
+    onSortChange(sortBy) {
+      let sortedLessons = []
+
+      switch (sortBy) {
+        case "ascending":
+          sortedLessons = this.reversed ? this.$store.state.lessons.reverse() : this.$store.state.lessons
+
+          if (this.reversed) this.reversed = false
+            
+          this.$store.commit("sortLessons", sortedLessons)
+
+          break
+        case "descending":
+          sortedLessons = this.$store.state.lessons.reverse()
+
+          this.reversed = true
+
+          this.$store.commit("sortLessons", sortedLessons)
+
+          break
+        case "subject":
+          sortedLessons = this.$store.state.lessons.sort((a, b) => {
+            if (a.subject.toUpperCase() < b.subject.toUpperCase()) {
+              return -1
+            }
+            if (a.subject.toUpperCase() > b.subject.toUpperCase()) {
+              return 1
+            }
+
+            return 0
+          })
+
+          this.$store.commit("sortLessons", sortedLessons)
+
+          break
+        case "location":
+          sortedLessons = this.$store.state.lessons.sort((a, b) => {
+            if (a.location.toUpperCase() < b.location.toUpperCase()) {
+              return -1
+            }
+            if (a.location.toUpperCase() > b.location.toUpperCase()) {
+              return 1
+            }
+
+            return 0
+          })
+
+          this.$store.commit("sortLessons", sortedLessons)
+
+          break
+        case "price":
+          sortedLessons = this.$store.state.lessons.sort((a, b) => a.price - b.price)
+
+          this.$store.commit("sortLessons", sortedLessons)
+
+          break
+        case "space":
+          sortedLessons = this.$store.state.lessons.sort((a, b) => a.space - b.space)
+
+          this.$store.commit("sortLessons", sortedLessons)
+
+          break
+        default:
+          break
+      }
     },
   },
 }
@@ -48,63 +121,66 @@ const Cart = {
       this.phone = 0
       this.resetCart()
       this.$toasted.success("Checkout successful.")
-      this.$router.push('/')
+      this.$router.push("/")
     },
   },
 }
 
 // Store (vuex)
 const store = new Vuex.Store({
-    state: {
-      lessons: [...lessons],
-      cart: [],
+  state: {
+    lessons: [...lessons],
+    cart: [],
+  },
+  mutations: {
+    addToCart(state, lessonItem) {
+      const index = state.lessons.findIndex((item) => item.subject === lessonItem.subject)
+      const isLessonInCart = state.cart.some((item) => item.subject === lessonItem.subject)
+
+      state.lessons[index].space -= 1
+
+      if (isLessonInCart) {
+        const cartIndex = state.cart.findIndex((item) => item.subject === lessonItem.subject)
+        state.cart[cartIndex].item += 1
+      } else {
+        state.lessons[index].item += 1
+        state.cart.push(state.lessons[index])
+      }
     },
-    mutations: {
-      addToCart(state, lessonItem) {
-        const index = state.lessons.findIndex((item) => item.subject === lessonItem.subject)
-        const isLessonInCart = state.cart.some((item) => item.subject === lessonItem.subject)
-  
-        state.lessons[index].space -= 1
-  
-        if (isLessonInCart) {
-          const cartIndex = state.cart.findIndex((item) => item.subject === lessonItem.subject)
-          state.cart[cartIndex].item += 1
-        } else {
-          state.lessons[index].item += 1
-          state.cart.push(state.lessons[index])
-        }
-      },
-      removeCart(state, cartItem) {
-        const index = state.cart.findIndex((item) => item.subject === cartItem.subject)
-        const lessonIndex = state.lessons.findIndex((item) => item.subject === cartItem.subject)
-  
-        state.lessons[lessonIndex].space += cartItem.item
-  
-        state.cart[index].item > 0 ? (state.cart[index].item -= 1) : (state.cart[index].item = 0)
-        state.cart.splice(index, 1)
-      },
-      resetCart(state) {
-        state.cart = []
-      },
+    removeCart(state, cartItem) {
+      const index = state.cart.findIndex((item) => item.subject === cartItem.subject)
+      const lessonIndex = state.lessons.findIndex((item) => item.subject === cartItem.subject)
+
+      state.lessons[lessonIndex].space += cartItem.item
+
+      state.cart[index].item > 0 ? (state.cart[index].item -= 1) : (state.cart[index].item = 0)
+      state.cart.splice(index, 1)
     },
-  })
-  
-  // Route
-  const routes = [
-    {path: "/index.html", component: Home},
-    {path: "/cart", component: Cart},
-  ]
-  
-  const router = new VueRouter({
-    mode: "history",
-    routes,
-  })
-  
-  // Vue-toasted
-  Vue.use(Toasted)
-  
-  // Initialize Vue
-  new Vue({
-    store,
-    router,
-  }).$mount("#app")
+    resetCart(state) {
+      state.cart = []
+    },
+    sortLessons(state, sortedLessons) {
+      state.lessons = [...sortedLessons]
+    },
+  },
+})
+
+// Route
+const routes = [
+  {path: "", component: Home},
+  {path: "/cart", component: Cart},
+]
+
+const router = new VueRouter({
+  mode: "history",
+  routes,
+})
+
+// Vue-toasted
+Vue.use(Toasted)
+
+// Initialize Vue
+new Vue({
+  store,
+  router,
+}).$mount("#app")
